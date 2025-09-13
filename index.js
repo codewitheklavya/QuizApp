c30d0e621886f4fc94c876a16f0d7e8eadb275a6
Eklavyasingh7 <codewitheklavya@gmail.com>
2025-09-13 16:58:34 +0530

    questions added

---
+++ index.js
@@ -0,0 +1,40 @@
+const questions = [
+    {
+        question: "Who was the first Avenger according to the MCU timeline?",
+        answer: [
+            {text: "thor", correct: false},
+            {text: "iron man", correct: false},
+            {text: "vicky", correct: false},
+            {text: "captain america", correct: true},
+        ]
+    },
+    {
+        question: "what is the spider man biggest struggle?",
+        answer: [
+            {text: "paying rent", correct: true},
+            {text: "propose to MJ", correct: false},
+            {text: "hanging from webs", correct: false},
+            {text: "back pain", correct: false},
+        ]
+    },
+    {
+        question: "Who said the famous line “We have a Hulk”?",
+        answer: [
+            {text: "Captain America", correct: false},
+            {text: "Lokesh", correct: false},
+            {text: "tonny stark", correct: true},
+            {text: "Amit", correct: false},
+        ]
+    },
+    {
+        question: "What nickname did Tony Stark give Thor?",
+        answer: [
+            {text: "jd", correct: false},
+            {text: "Buddy", correct: false},
+            {text: "point break", correct: true},
+            {text: "hammer king", correct: false},
+        ]
+    },
+]
+
+
