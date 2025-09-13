096ff62452bbb26b8c6cb51c996d9d71615ea196
Eklavyasingh7 <codewitheklavya@gmail.com>
2025-09-13 17:54:03 +0530

    array added and hover disabled

--- index.js
+++ index.js
@@ -103,6 +103,13 @@ function selectAnswer(e){
     }else{
         selectedBtn.classList.add("incorrect"); 
     }
+    Array.from(answerButton.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
 }
 
 startquiz();
